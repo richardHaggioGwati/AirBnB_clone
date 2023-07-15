a1b70929f61c44ba750e649aa4a1ceffbe4c3d16
Richard Gwati <haggio0844@gmail.com>
2023-07-15 13:56:59 +0200

    beta_console

--- console.py
+++ console.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 """Module for the entry point of the command interpreter."""
 
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program with Ctrl+D (EOF)"""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
