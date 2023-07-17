110d4e9d14d05e9058b25b24d813250db8e01996
Richard Gwati <haggio0844@gmail.com>
2023-07-17 11:02:10 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -7,32 +7,22 @@ from models import storage
 
 
 class BaseModel:
+
     """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes
+        """Initializes instance attributes"""
 
-        Args:
-            *args: list of arguments
-            **kwargs: dict of key-value arguments
-        """
-
-        if kwargs:
-            created_at = kwargs.pop("created_at", None)
-            updated_at = kwargs.pop("updated_at", None)
-
-            if created_at:
-                self.__dict__["created_at"] = datetime.strptime(
-                    created_at, "%Y-%m-%dT%H:%M:%S.%f"
-                )
-
-            if updated_at:
-                self.__dict__["updated_at"] = datetime.strptime(
-                    updated_at, "%Y-%m-%dT%H:%M:%S.%f"
-                )
-
-            for key, value in kwargs.items():
-                self.__dict__[key] = value
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -42,10 +32,11 @@ class BaseModel:
     def __str__(self):
         """Returns official string representation"""
 
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates the public instance attribute updated_at"""
+        """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
         storage.save()
